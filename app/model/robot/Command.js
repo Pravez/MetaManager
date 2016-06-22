f54353fe768287d16d995362eb84520548d49b32
Pravez <paul.ravezb@gmail.com>
2016-06-22 19:27:03 +0200

    Continued with utilities with commandsTypes, and types

--- app/model/robot/Command.js
+++ app/model/robot/Command.js
@@ -1,1 +1,27 @@
 "use strict";
+
+var CommandType = require('../utility/CommandType');
+
+class Command{
+    constructor(commandType, args){
+        if(commandType && typeof commandType === "string"){
+            this._command = CommandType.get(commandType);
+        }else {
+            this._command = commandType;
+        }
+
+        this._args = args;
+    }
+
+    execute(){
+        return this._command.cmd + " " + this._args.toString();
+    }
+
+    getCommandType(){
+        return this._command;
+    }
+}
+
+
+console.log(new Command('dx', 20).execute());
+module.exports = Command;
---
+++ app/model/utility/Types.js
@@ -0,0 +1,9 @@
+var Types = {
+    INT: "INTEGER",
+    FLOAT: "FLOAT",
+    BOOL: "BOOLEAN",
+    SINGLE_VALUE: "SINGLE VALUE",
+    NO_TYPE: "NO TYPE"
+};
+
+module.exports = Types;
